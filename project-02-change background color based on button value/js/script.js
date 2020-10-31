7cf4cbfc7bf00a1a179a2970a2c63a92d2181e7e
r-tavakoli <42064303+r-tavakoli@users.noreply.github.com>
2020-10-31 15:48:37 +0330

    updated

--- project-02-change background color based on button value/js/script.js
+++ project-02-change background color based on button value/js/script.js
@@ -1,20 +1,11 @@
-//Changing Background Color
+//Changing background color based on button's color
 
-const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['#16697a','#db6400','#ffa62b','#f8f1f1']
+body.style.backgroundColor = 'white'
 
-body.style.backgroundColor = '#f1f6f9'
-button.addEventListener('click',changeBackground)
 
-var colorIndex = 0
-function changeBackground(){
-    if (colorIndex==parseInt(colors.length)){
-        colorIndex = 0
-    }
-    else{
-        body.style.backgroundColor=colors[colorIndex]
-        colorIndex++
-    }
-}
+$('.btn').click(function(){
+    let color = this.style.backgroundColor;
+    body.style.backgroundColor = color;
+})
 
